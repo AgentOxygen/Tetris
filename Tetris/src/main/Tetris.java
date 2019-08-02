8a15d51797bd9fc2b6cc8a678cd2a8baf27a9e3a
Cameron <cameronscummins@gmail.com>
2019-08-02 16:27:23 -0500

    Updated to implement block manager.

--- Tetris/src/main/Tetris.java
+++ Tetris/src/main/Tetris.java
@@ -4,52 +4,32 @@ import java.util.Random;
 
 public class Tetris implements Runnable{
 	
-	/**Creates grid to be read for updating the visuals.
-	 * 0 draws a blank square
-	 * 1 draws a black-filled square
-	 * */
-	public int[][] grid;
-	/**Dictates width of grid*/
-	public int width;
-	/**Dictates height of grid*/
-	public int height;
-	/*Regulates game loop**/
+	/**Creates BlockManager to be read for updating the visuals and game*/
+	public BlockManager bm;
+	/**Regulates game loop*/
 	public boolean update = true;
 	/**Sets rate, in updates per second, of update loop*/
 	public double rate = 30;
 	/**Creates visuals for game.*/
 	public Visuals vis;
 	/*Used for timing in seconds**/
-	public double game_time = 0.0;
+	public static double game_time = 0.0;
 	/**Creates game instance with grid size set by provided width and height variables. Use .run() to start update loop.*/
-	public Tetris(int width_, int height_) {
-		width = width_;
-		height = height_;
-		grid = new int[width][height];
-		for(int w = 0; w < width; w++) {
-			for(int h = 0; h < height; h++) {
-				grid[w][h] = 0;
-			}
-		}
+	public Tetris(int width, int height) {
+		bm = new BlockManager(width, height);
 		vis = new Visuals(30.0, width, height);
 		Thread game = new Thread(this);
 		game.start();
 	}
 	
-	private double time_switch = 0;
-	
 	/**Called during each update.*/
 	public void update() {
 		
-		//Randomly selects cells to fill, just a proof of concept
-		if(game_time - time_switch >= 0) {
-			Random r = new Random();
-			grid[r.nextInt(width)][r.nextInt(height)] = 1;
-			time_switch = game_time;
-		}
+		//Update Block Manager
+		bm.update();
 		
 		//Update visuals
-		vis.graph = grid;
+		vis.graph = bm.getGrid();
 	}
 	
 	private void loop() {
