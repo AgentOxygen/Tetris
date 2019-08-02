3d840a3864515907a525e90c4d1d1a236cb4ef66
Cameron <cameronscummins@gmail.com>
2019-08-02 16:28:39 -0500

    Proof of concept with game_clock, visuals update loop enabled.

--- Tetris/src/main/Init.java
+++ Tetris/src/main/Init.java
@@ -3,6 +3,6 @@ package main;
 public class Init {
 	public static void main(String args[]) {
 		System.out.println("Game of Tetris Starting...");
-		Tetris game = new Tetris(10, 20);
+		new Tetris(10, 20);
 	}
 }
