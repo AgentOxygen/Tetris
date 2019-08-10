fabd9542d3c66c2a1ce42e77542d5244e5d04188
Cameron <cameronscummins@gmail.com>
2019-08-09 23:04:52 -0500

    Removed old print.

--- Tetris/src/main/Init.java
+++ Tetris/src/main/Init.java
@@ -3,7 +3,6 @@ package main;
 public class Init {
 	public static void main(String args[]) {
 		System.out.println("Game of Tetris Starting...");
-		System.out.print("ijabiuwabgiuagwbbiuawg");
 		new Tetris(10, 20);
 	}
 }
